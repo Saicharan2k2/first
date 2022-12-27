d755337aeb50596023d0354f67ec9ce0ed48288f
Saicharan2k2 <saicharankodam1234@gmail.com>
2022-12-28 01:22:43 +0530

    first commit

---
+++ first/src/first/Launch3.java
@@ -0,0 +1,21 @@
+package first;
+
+public class Launch3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int n=6;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
+
